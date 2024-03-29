8746b83edf1e36aec8b8f8ef5a106506e69c4a9d
Toney <ta.winston@outlook.com>
2024-03-29 13:37:56 -0500

    Added spotImages

--- app/models/spot.py
+++ app/models/spot.py
@@ -1,9 +1,10 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
 
 class Spot(db.Model):
   __tablename__ = 'spots'
 
-  if environment == "production":
+  if environment == 'production':
     __table_args__ = {'schema': SCHEMA}
 
   id = db.Column(db.Integer, primary_key=True)
@@ -12,3 +13,34 @@ class Spot(db.Model):
   address = db.Column(db.String(150), nullable=False)
   location = db.Column(db.String(150))
   universe = db.Column(db.String(150), nullable=False)
+  lat = db.Column(db.FLOAT, nullable=False)
+  lng = db.Column(db.FLOAT, nullable=False)
+  description = db.Column(db.String(255))
+  price = db.Column(db.FLOAT, nullable=False)
+  created_at = db.Column(db.DateTime, default=datetime.now())
+
+  owner = db.relationship(
+    'User',
+    back_populates='spots'
+  )
+
+  images = db.relationship(
+    'SpotImage',
+    back_populates='spot'
+  )
+
+  def to_dict(self):
+    return {
+      'id': self.id,
+      'ownerId': self.owner_id,
+      'name': self.name,
+      'address': self.address,
+      'location': self.location,
+      'universe': self.universe,
+      'lat': self.lat,
+      'lng': self.lng,
+      'description': self.description,
+      'price': self.price,
+      'createdAt': self.created_at,
+      'images': [image.to_dict() for image in self.images]
+    }
---
+++ app/models/spot_image.py
@@ -0,0 +1,40 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from .aws_helper import remove_file_from_s3
+from sqlalchemy import event
+
+class SpotImage(db.Model):
+  __tablename__ = 'spot_images'
+
+  if environment == 'production':
+    __table_args__ = {'schema': SCHEMA}
+
+  id = db.Column(db.Integer, primary_key=True)
+  spot_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('spots.id')), nullable=False)
+  url = db.Column(db.String(300), nullable=False)
+  preview = db.Column(db.Boolean, default=True)
+
+  spot = db.relationship(
+    'Spot',
+    back_populates='images'
+  )
+
+  def to_dict(self):
+    return {
+      'id': self.id,
+      'url': self.url,
+      'preview': self.preview
+    }
+
+def check_preview(mapper, connection, target):
+    if target.preview:
+      images = SpotImage.query.all()
+      for image in images:
+        image.preview = False
+
+def on_delete(mapper, connection, target):
+    remove_file_from_s3(target.url)
+    return 'done'
+
+event.listen(SpotImage, 'before_insert', check_preview)
+
+event.listen(SpotImage, 'before_delete', on_delete)
