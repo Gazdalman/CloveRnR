69ac3fd49ba02e37ebb5388152834e27d37d6f55
Toney <ta.winston@outlook.com>
2024-04-05 09:15:45 -0500

    Beginning Forms

---
+++ app/forms/create_spot.py
@@ -0,0 +1,6 @@
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired, Length
+from wtforms import StringField, FloatField, TextAreaField, FileField, DateField, BooleanField
+
+class CreateSpotForm(FlaskForm):
+  pass
