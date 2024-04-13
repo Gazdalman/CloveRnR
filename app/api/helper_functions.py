2aed7b782c5bd954d73a27b5342ee1c4e21b6eaf
Toney <ta.winston@outlook.com>
2024-04-13 07:48:53 -0500

    Finished booking helper function

--- app/api/helper_functions.py
+++ app/api/helper_functions.py
@@ -25,3 +25,7 @@ def check_bookings(start,end,id):
       return 'End date conflicts with an existing booking'
     if date1 == end:
       return 'End date conflicts with an existing booking'
+    if date1 <= start and date2 >= end:
+      return 'Booking dates conflict with an existing booking'
+    if start <= date1 and end >= date2:
+      return 'Booking dates conflict with an existing booking'
