ecf9fdb4a9e78e00983af6942f110afac63d5340
Toney <ta.winston@outlook.com>
2024-06-08 21:44:20 -0500

    Fixed booking form not pushing

--- app/forms/booking_form.py
+++ app/forms/booking_form.py
@@ -1,3 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired, Length, NumberRange
 from wtforms import DateTimeField, TextAreaField
+
+class BookingForm(FlaskForm):
+  start = DateTimeField('start', validators=[DataRequired()])
+  end = DateTimeField('end', validators=[DataRequired()])
+  notes = TextAreaField('notes', validators=[Length(max=255)])
