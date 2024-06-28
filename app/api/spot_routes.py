65f92db5ac88ff7e957ffa0ee1839f2189e2e6bf
Toney <ta.winston@outlook.com>
2024-06-27 21:31:36 -0500

    Added two more routes for spots "

--- app/api/spot_routes.py
+++ app/api/spot_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
 from app.models import Review, Spot, Booking, SpotImage
-from app.forms import ReviewForm, BookingForm
+from app.forms import ReviewForm, BookingForm, CreateSpotForm
 
 spot_routes = Blueprint('reviews', __name__)
 
@@ -9,3 +9,20 @@ spot_routes = Blueprint('reviews', __name__)
 def get_all_spots():
   spots = Spot.query.all()
   return {spot.id: spot.to_dict() for spot in spots}
+
+@spot_routes.route('/<int:id>')
+def get_spot(id):
+  spot = Spot.query.get(id)
+  if spot:
+    return spot.to_dict()
+  else:
+    return {'errors': 'Spot not found'}, 404
+
+@spot_routes.route('/create', methods=['POST'])
+@login_required
+def create_spot():
+  form = CreateSpotForm()
+  form['csrf_token'].data = request.cookies['csrf_token']
+
+  if form.validate_on_submit():
+    data = form.data
--- app/forms/create_spot.py
+++ app/forms/create_spot.py
@@ -4,9 +4,9 @@ from wtforms import StringField, FloatField, TextAreaField
 
 class CreateSpotForm(FlaskForm):
   name = StringField('name', validators=[DataRequired(), Length(min=3,max=50)])
-  # address = StringField('address', validators=[DataRequired(), Length(max=100)])
-  location = StringField('location', validators=[DataRequired(), Length(max=100)])
-  universe = StringField('location', validators=[DataRequired(), Length(max=150)])
+  address = StringField('address', validators=[DataRequired(), Length(max=100)])
+  city = StringField('location', validators=[DataRequired(), Length(max=100)])
+  state = StringField('location', validators=[DataRequired(), Length(max=150)])
   lat = FloatField('lat', validators=[DataRequired(), NumberRange(min=-90,max=90)])
   lng = FloatField('lng', validators=[DataRequired(), NumberRange(min=-180,max=180)])
   description = TextAreaField('description', validators=[Length(max=250)])
