4b07fa75771a643821337096312655b1ad0f3383
Toney <ta.winston@outlook.com>
2024-07-03 16:02:22 -0500

    Added a new booking helper function

--- app/api/helper_functions.py
+++ app/api/helper_functions.py
@@ -1,5 +1,5 @@
 from flask_login import current_user
-from app.models import Spot
+from app.models import Spot, Booking
 from datetime import datetime
 
 def user_owns_spot(record):
@@ -14,6 +14,29 @@ def check_bookings(start,end,id):
   bookings = spot.bookings
 
   for booking in bookings:
+    date1 = booking.start
+    date2 = booking.end
+
+    if date1 <= start and date2 >= start:
+      return 'Start date conflicts with an existing booking'
+    if date1 == start or date2 == start:
+      return 'Start date conflicts with an existing booking'
+    if date1 <= end and date2 >= end:
+      return 'End date conflicts with an existing booking'
+    if date1 == end:
+      return 'End date conflicts with an existing booking'
+    if date1 <= start and date2 >= end:
+      return 'Booking dates conflict with an existing booking'
+    if start <= date1 and end >= date2:
+      return 'Booking dates conflict with an existing booking'
+  return 'ok'
+
+def edit_bookings_check(start,end,id):
+  booking = Booking.query.get(id)
+
+  spot_bookings = Booking.query.filter(Booking.spot_id==booking.spot_id, Booking.id!=id).all()
+
+  for booking in spot_bookings:
     date1 = booking.start
     date2 = booking.end
 
