ba3512e2df4412dff03e0470b47eb300211a1402
Toney <ta.winston@outlook.com>
2024-07-04 12:39:09 -0500

    Added review route

---
+++ app/api/booking_routes.py
@@ -0,0 +1,65 @@
+from flask import Blueprint, jsonify, request
+from flask_login import login_required, current_user
+from app.models import Review, Booking, db
+from app.forms import ReviewForm, BookingForm
+from .helper_functions import edit_bookings_check
+from datetime import datetime
+
+booking_routes = Blueprint('bookings', __name__)
+
+def validation_errors_to_error_messages(validation_errors):
+  """
+  Simple function that turns the WTForms validation errors into a simple list
+  """
+  errorMessages = []
+  for field in validation_errors:
+    for error in validation_errors[field]:
+      errorMessages.append(f'{field} : {error}')
+  return errorMessages
+
+@booking_routes.route('/<int:id>/edit', methods=['PUT'])
+@login_required
+def edit_booking(id):
+  booking = Booking.query.get(id)
+
+  form = BookingForm()
+  form['csrf_token'].data = request.cookies['csrf_token']
+
+  if not booking:
+    return 'No such booking found', 404
+
+  if booking.user_id != current_user.get_id():
+    return 'You are not authorized', 403
+
+  if form.validate_on_submit():
+    data = form.data
+
+    good_dates = edit_bookings_check(data['start'], data['end'],id)
+
+    if good_dates  != 'ok':
+      return good_dates
+
+    booking.start = data['start']
+    booking.end = data['end']
+    booking.notes = data['notes']
+
+    db.session.commit()
+    return booking.to_dict()
+
+  return {'errors': validation_errors_to_error_messages(form.errors)}, 400
+
+@booking_routes.route('/<int:id>/review', methods=['POST'])
+@login_required
+def create_review(id):
+  booking = Booking.query.get(id)
+
+  form = ReviewForm()
+
+  if not booking:
+    return 'No booking found', 404
+
+  if booking.user_id != current_user.get_id():
+    return 'You are not authorized', 403
+
+  if booking.start > datetime.now():
+    return 'Can\'t review a future stay', 403
--- app/api/spot_routes.py
+++ app/api/spot_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
 from .helper_functions import user_owns_spot, check_bookings
 from app.models import Review, Spot, Booking, SpotImage, db
-from app.forms import ReviewForm, BookingForm, CreateSpotForm, SpotImageForm, EditSpotForm
+from app.forms import BookingForm, CreateSpotForm, SpotImageForm, EditSpotForm
 from app.api.aws_helper import upload_file_to_s3, get_unique_filename
 
 spot_routes = Blueprint('spots', __name__)
@@ -191,7 +191,8 @@ def add_booking(id):
       user_id=current_user.get_id(),
       spot_id=id,
       start=data['start'],
-      end=data['end']
+      end=data['end'],
+      notes=data['notes']
     )
 
     db.session.add(booking)
